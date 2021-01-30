13cbffae441a435f0dad78e6a04f1749cf8eb59f
filip91 <filip.delibasic12@gmail.com>
2021-01-30 18:05:29 +0100

    init project

---
+++ src/components/News/News.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./News.css";
+
+class News extends Component {
+  state = {};
+  render() {
+    return <div>News</div>;
+  }
+}
+
+export default News;
